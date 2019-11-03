cc2a2d44f8945e46e3e75e4f988fea4619f480d1
Florentina Diaconescu <fdiaconescu@hotmail.com>
2019-11-03 08:53:08 -0800

    JPA

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -1,32 +1,46 @@
 package guru.springframework.spring5webapp.bootstrap;
 
-import guru.springframework.spring5webapp.model.Author;
-import guru.springframework.spring5webapp.model.Book;
-import guru.springframework.spring5webapp.repositories.AuthorRepository;
-import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
-        this.authorRepository = authorRepository;
-        this.bookRepository = bookRepository;
-    }
+   
 
-    @Override
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository,
+			PublisherRepository publisherRepository) {
+		super();
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
+	}
+
+	@Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         initData();
     }
 
     private void initData() {
+    	Publisher publisher = new Publisher();
+    	publisher.setName("Editura Culturala");
+    	publisher.setAddress("Bucuresti");
+    	publisherRepository.save(publisher);
+    	
         Author eminescu = new Author("Mihai", "Eminescu");
-        Book poetryBook = new Book("Poezii", "1234", "Editura Culturala");
+        Book poetryBook = new Book("Poezii", "1234", publisher);
         eminescu.getBooks().add(poetryBook);
         poetryBook.getAuthors().add(eminescu);
 
@@ -38,7 +52,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
 
         Author creanga = new Author("Ion", "Creanga");
-        Book amintiri = new Book("Amintiri din copilarie", "2345", "Cultura Romana");
+        Book amintiri = new Book("Amintiri din copilarie", "2345", publisher);
         creanga.getBooks().add(amintiri);
         //amintiri.getAuthors().add(creanga);
 
